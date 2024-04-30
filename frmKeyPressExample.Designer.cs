5cee13dc690082db14fae5cc1a5dd0654e4f681e
EthFox <168486262+EthFox@users.noreply.github.com>
2024-04-30 18:15:53 -0400

    Add project files.

---
+++ frmKeyPressExample.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace KeyPressedPresentationExample
+{
+	partial class frmKeyPressExample
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.movableSquare = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.movableSquare)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// movableSquare
+			// 
+			this.movableSquare.BackColor = System.Drawing.Color.DodgerBlue;
+			this.movableSquare.Location = new System.Drawing.Point(352, 190);
+			this.movableSquare.Name = "movableSquare";
+			this.movableSquare.Size = new System.Drawing.Size(50, 50);
+			this.movableSquare.TabIndex = 0;
+			this.movableSquare.TabStop = false;
+			// 
+			// frmKeyPressExample
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.movableSquare);
+			this.KeyPreview = true;
+			this.Name = "frmKeyPressExample";
+			this.Text = "KeyPress Example";
+			this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.frmKeyPressExample_KeyPress);
+			((System.ComponentModel.ISupportInitialize)(this.movableSquare)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox movableSquare;
+	}
+}
+
---
+++ frmKeyPressExample.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyPressedPresentationExample
+{
+	public partial class frmKeyPressExample : Form
+	{
+		public frmKeyPressExample()
+		{
+			InitializeComponent();
+		}
+
+		private void frmKeyPressExample_KeyPress(object sender, KeyPressEventArgs e)
+		{                  // e represents the KeyPressEventArgs ^
+			char KeyPressed = e.KeyChar; // get the key pressed as a character
+			Point squareLoc = movableSquare.Location; // get the location of the player's square
+			// !!! Have to use a seperate point as the offset method does NOT reference the object, rather it just creates a copy of the location point !!! \\
+			switch (KeyPressed) // switch case is an easy way to test for different key presses
+			{
+				case 'w':
+					squareLoc.Offset(0, -50); // move the point the desired amount in the desired direction
+					break;
+				case 'a':
+					squareLoc.Offset(-50, 0);
+					break;
+				case 's':
+					squareLoc.Offset(0, 50);
+					break;
+				case 'd':
+					squareLoc.Offset(50, 0);
+					break;
+				case '\u001b': // keys that do not have an equivalent char are stored in unicode, this is the 'escape' key
+					this.Close(); // close the form
+					break;
+			}
+			movableSquare.Location = squareLoc; // move the player's square to the offset point
+		}
+	}
+}
